c2cde55cd3f487eccf9edd48ceab4e9216107a04
Vinicius <vinny.ast95@gmail.com>
2020-04-02 14:47:54 -0300

    Following the Alaska design

--- device.mk
+++ device.mk
@@ -1,35 +1,37 @@
 LOCAL_PATH := device/vernee/apollo_x
-# TODO
-#Overlays
-#DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1929
-TARGET_SCREEN_WIDTH := 1080
- 
-# Device uses high-density artwork where available
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
+
+# Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi 280dpi hdpi tvdpi mdpi ldpi
 
-# TODO
 # Recovery allowed devices
 TARGET_OTA_ASSERT_DEVICE := x,apollo_x,k15tb_a
 
-# TODO
-# Lights
-PRODUCT_PACKAGES += \
-    lights.mt6797
 
-# TODO
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_KERNEL):kernel
+
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml
 
-PRODUCT_PACKAGES += \
-    libxml2
-
-# TODO
 # Media
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
@@ -39,48 +41,29 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/factory_init.connectivity.rc:root/factory_init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
     $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6797:root/fstab.mt6797 \
-    $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6797.rc:root/init.mt6797.rc \
     $(LOCAL_PATH)/rootdir/init.mt6797.usb.rc:root/init.mt6797.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.sensor.rc:root/init.sensor.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.connectivity.rc:root/meta_init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
     $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
     $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6797.rc:root/ueventd.mt6797.rc
 
-# TODO
 # TWRP
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
 
-# TODO
-# Fingerprint
-PRODUCT_PACKAGES += \
-    fingerprintd
+# Device product elements
+include $(LOCAL_PATH)/product/*.mk
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+# Dalvik heap configurations
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-4096-dalvik-heap.mk)
 
-# TODO
-# Vulkan
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+# Call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-4096-hwui-memory.mk)
 
-# Dalvik/HWUI
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
-# TODO
-# Common stuff
-$(call inherit-product, vendor/mad/config/common.mk)
-
-# TODO: Adding vendor files
 # Vendor
-#$(call inherit-product, vendor/vernee/apollo_x/apollo_x-vendor.mk)
+$(call inherit-product, vendor/vernee/apollo_x/apollo_x-vendor.mk)
--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,3 @@
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from the common Open Source product configuration
@@ -6,21 +5,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/vernee/apollo_x/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 
 # Release name
-PRODUCT_RELEASE_NAME := Apollo X
+PRODUCT_RELEASE_NAME := apollo_x
 
-PRODUCT_DEVICE := X
+PRODUCT_DEVICE := apollo_x
 PRODUCT_NAME := lineage_apollo_x
 PRODUCT_BRAND := Vernee
-PRODUCT_MODEL := Apollo X
+PRODUCT_MODEL := Apollo_x
 PRODUCT_MANUFACTURER := Vernee
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
@@ -29,7 +27,3 @@ TARGET_SCREEN_HEIGHT      := 1920
 TARGET_SCREEN_WIDTH       := 1080
 TARGET_BOOTANIMATION_NAME := 1080
 
-#Use the latest approved GMS identifiers unless running a signed build
-#ifneq ($(SIGN_BUILD),true)
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#	BUILD_FINGERPRINT=
