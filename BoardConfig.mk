3b8ea8f6b2a44fe6c8ca3169b5fb85bfcce2fb84
vinixast <63014441+vinixast@users.noreply.github.com>
2020-04-17 02:26:47 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,10 +42,10 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x04f88000 --second_offset 0x00e88000 --tags_offset 0x03f88000 --board K15TB-A.A.vR15
-TARGET_KERNEL_SOURCE := kernel/vernee/apollo_lite
+TARGET_KERNEL_SOURCE := kernel/vernee/apollo_x
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CONFIG := mad_val_n1_defconfig
+TARGET_KERNEL_CONFIG := k15tb_a_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 MTK_APPENDED_DTB_SUPPORT := yes
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
